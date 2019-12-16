8928273c29c101a7b5a8b78dca6e03d728b031c5
pachniar <01130999@pw.edu.pl>
2019-12-16 22:05:28 +0100

    Testy dodawania lotów

--- src/backend/src/main/test/java/pl/pw/edu/demo/BDD/flights/FlightScenarioSteps.java
+++ src/backend/src/main/test/java/pl/pw/edu/demo/BDD/flights/FlightScenarioSteps.java
@@ -1,4 +1,58 @@
 package pl.pw.edu.demo.BDD.flights;
 
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Named;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+import pl.pw.edu.demo.algorithm.Graph;
+import pl.pw.edu.demo.algorithm.Vertex;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class FlightScenarioSteps {
+    private Graph graph;
+    private String startCity;
+    private String destCity;
+    private double price;
+    private double time;
+
+    @Given("the cities is <cities>")
+    public void createGraphWithCities(@Named("cities") String cities) {
+        String[] cityList = cities.split(",");
+        graph = new Graph();
+        for(String cit: cityList){
+            graph.addVertex(cit);
+        }
+    }
+
+    @When("startCity is <startCity>")
+    public void setStartCity(@Named("startCity") String start) {
+        startCity = start;
+    }
+
+    @When("destCity is <destCity>")
+    public void setDestCity(@Named("destCity") String dest) {
+        destCity = dest;
+    }
+
+    @When("price is <price>")
+    public void setPrice(@Named("price") double pr) {
+        price = pr;
+    }
+
+    @When("time is <time>")
+    public void setTime(@Named("test") double tim) {
+        time = tim;
+    }
+
+    @Then("the Algoritm should return <result>")
+    public void checkCities(@Named("result") String res) {
+        String result = res;
+        String response = graph.addFlight(startCity, destCity, price, time);
+        assertEquals(result, response);
+    }
 }
--- src/backend/src/main/test/java/pl/pw/edu/demo/BDD/flights/flight_scenario.story
+++ src/backend/src/main/test/java/pl/pw/edu/demo/BDD/flights/flight_scenario.story
@@ -1,7 +1,6 @@
-Narrative: Użytkownik chce dodać połączenie lotnicze pomiedzy miastami
-
 Scenario: W bazie są niepołączone miasta
-Given the connections is <cities>
+
+Given the cities is <cities>
 When startCity is <startCity>
 When destCity is <destCity>
 When price is <price>
@@ -9,9 +8,9 @@ When time is <time>
 Then the Algoritm should return <result>
 
 Examples:
-|cities                  |startCity   |destCity   |time    |price    |result
-|Warszawa, Legnica       |Warszawa    |Legnica    |10      |10       |ok
-|Warszawa, Legnica       |Radom       |Legnica    |10      |10       |Miasto Radom nie zostało wprowadzone
-|Warszawa, Legnica       |Warszawa    |Radom      |10      |10       |Nie odnaleziono połączenia z miasta Warszawa do Radom
-|Warszawa, Legnica       |Warszawa    |Legnica    |-10     |10       |Czas -10 jest niepoprawny
-|Warszawa, Legnica       |Warszawa    |Legnica    |10      |-10      |Cena -10 jest niepoprawna
+cities           |startCity |destCity |time |price |result                                                  |
+Warszawa,Legnica |Warszawa  |Legnica  |10   |10    |ok                                                      |
+Warszawa,Legnica |Radom     |Legnica  |10   |10    |Miasto Radom nie zostało wprowadzone                   |
+Warszawa,Legnica |Warszawa  |Radom    |10   |10    |Nie odnaleziono połączenia z miasta Warszawa do Radom |
+Warszawa,Legnica |Warszawa  |Legnica  |-10  |10    |Czas -10 jest niepoprawny                               |
+Warszawa,Legnica |Warszawa  |Legnica  |10   |-10   |Cena -10 jest niepoprawna                               |
